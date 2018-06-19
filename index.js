c649cba67bd5b4884b2865dae32a99bd49ba3dcf
fredsiika <fredsiika@gmail.com>
2018-06-19 12:31:45 -0500

    Created entry point for webapp index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Primary file for API 
+ * Description: Entry point of Web App and utilized by Parcel(https: //parceljs.org/).
+ * Parcel can take any type of file as an entry point, but an HTML or JavaScript is a good place to start.
+ * Parcel has a development server built in , which will automatically rebuild your app as you change files and supports hot module replacement
+ * for fast development.Just point it at your entry file:
+ *  
+ * Author: Fred Siika 
+ * Date created: 19-Jun-2018
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./lib/config');
+const fs = require('fs');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
